19e3a5472e7ef96814da5f7064e576574bcf8c9c
Jacob Baskin <jacob.baskin@gmail.com>
2014-11-08 12:23:21 -0500

    Added video object

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
-$(document).ready(function(){
+function video(screenshot, depth, seq) {
+  this.dom = $('<div/>');
+  this.img = $('<img/>');
+  this.img.attr('src', screenshot).appendTo(this.dom);
+  // TODO: mask
+  this.depth = depth;
+  this.seq = seq;
+}
+video.prototype.show = function(cur_seq, cur_depth) {
+  this.cur_seq = cur_seq;
+  this.cur_depth = cur_depth;
+  this.setPos();
+  this.dom.appendTo($('body'));
+}
+video.prototype.setCurSeq = function(cur_seq) {
+  this.cur_seq = cur_seq;
+  this.setPos();
+}
+video.prototype.setCurDepth = function(cur_depth) {
+  this.cur_depth = cur_depth;
+  this.setPos();
+}
 
+VIDEOS = [];
+
+$(document).ready(function(){
 });
+
+
