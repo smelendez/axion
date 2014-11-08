2ee049dbfe2d820bac2923a08ce09a86266ce95d
Jacob Baskin <jacob.baskin@gmail.com>
2014-11-08 15:35:33 -0500

    Hook up clips to video player. Add code for getting the next clip when one finishes.

--- js/player.js
+++ js/player.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-  PLAYER = Popcorn('#player');
+  window.PLAYER = Popcorn('#player');
+  PLAYER.on('ended', function() {
+    var nextMedia = CLIPS.playNext();
+    $('#player').attr('src', nextMedia);
+  });
+    $('#player').attr('src', CLIPS.playNext());
 });
