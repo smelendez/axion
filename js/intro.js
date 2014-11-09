5962f742363e3bc98016448f2865c404ebd0ec5f
Jacob Baskin <jacob.baskin@gmail.com>
2014-11-08 20:32:40 -0500

    Intro done (for now).

--- js/intro.js
+++ js/intro.js
@@ -1,5 +1,20 @@
 window.INTRO = true;
 
+function clickUp() {
+  $('#intro .fa-arrow-up').css('background', '#666');
+  setTimeout(function() {
+    $('#intro .fa-arrow-up').css('background', '#333');
+  }, 250);
+  DEPTHMETER.decrementDepth();
+}
+function clickDown() {
+  $('#intro .fa-arrow-down').css('background', '#666');
+  setTimeout(function() {
+    $('#intro .fa-arrow-down').css('background', '#333');
+  }, 250);
+  DEPTHMETER.incrementDepth();
+}
+
 function stage1() {
   $('#stage1').css('opacity', '1');
   setTimeout(stage2, 5000);
@@ -8,6 +23,10 @@ function stage1() {
 function stage2() {
   $('#stage1').css('opacity', '0');
   $('#stage2').css('opacity', '1');
+  setTimeout(clickUp, 5000);
+  setTimeout(clickUp, 7000);
+  setTimeout(clickDown, 9000);
+  setTimeout(clickDown, 11000);
   setTimeout(stage3, 15000);
   // Use your up and down arrow keys to adjust the depth
 }
@@ -15,12 +34,6 @@ function stage3() {
   $('#stage2').css('opacity', '0');
   $('#stage3').css('opacity', '1');
   // Use the space bar to play and stop the video
-  setTimeout(stage4, 15000);
-}
-function stage4() {
-  $('#stage3').css('opacity', '0');
-  $('#stage4').css('opacity', '1');
-  // Press space to begin 
 }
 
 $(document).ready(stage1);  
